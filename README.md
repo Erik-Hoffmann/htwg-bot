c3e8ff66bfee49da81207c1f7557032d35c89d84
Erik-Hoffmann <erikhoff.1@web.de>
2022-05-21 15:14:54 +0200

    dockerize + fix date bug

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # htwg-bot
 Discord bot with commands related to htwg Constance
+
+``docker build -t htwg-bot .``
+
+``docker run -it --rm --name htwg-bot htwg-bot -d``
--- src/bot.py
+++ src/bot.py
@@ -5,6 +5,7 @@ import random
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import date
 
 from bs4 import BeautifulSoup
 import requests
@@ -44,21 +45,38 @@ async def nine_nine(ctx):
 async def menu(ctx):
     page_response = requests.get(MENSA_HTWG)
     soup = BeautifulSoup(page_response.content, 'html.parser')
-    contents = soup.find('div', class_='contents_aktiv')
-    menus = contents.find_all('div', class_='speiseplanTagKat')
+    contents = soup.find('div', class_='tx-speiseplan')
+    date_tabs = contents.find_all('a', class_='tab')
+    current_tab = None
+    current_tab_class = None
+    for tab in date_tabs:
+        current_tab = tab.text
+        if date.today().strftime("%d.%m.") in current_tab:
+        # if "16.05." in current_tab:
+            current_tab_class = tab.get('class')[1]
+            break
     
+            
     response = discord.Embed(
                 title="In der Mensa gibt es:",
                 url=MENSA_HTWG
             )
-    for menu in menus:
-        category=menu.find('div', class_='category')
-        food=menu.find('div', class_='title_preise_1').find('div', class_='title')
-        for sup in food.select('sup'):
-            if not validateIngr(sup.text): sup.unwrap()
-            else : sup.decompose()
-        response.add_field(name=category.text, value=f"{food.text})", inline=True)
-    
+    if not current_tab_class is None:
+
+        day = contents.find('div', {"id":current_tab_class})
+        menus = day.find_all('div', class_='speiseplanTagKat')
+
+
+        for menu in menus:
+            category=menu.find('div', class_='category')
+            food=menu.find('div', class_='title_preise_1').find('div', class_='title')
+            for sup in food.select('sup'):
+                if not validateIngr(sup.text): sup.unwrap()
+                else : sup.decompose()
+            response.add_field(name=category.text, value=f"{food.text}", inline=True)
+    else:
+        response.add_field(name="Heute wohl nix", value="Zu oder so :(")
+
     await ctx.send(embed=response)
 
 
