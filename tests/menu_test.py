1835a604b9da8bfb9db6450a76c64ac1b16c1755
Erik-Hoffmann <erikhoff.1@web.de>
2022-06-28 22:13:50 +0200

    show food attribute + convenience stuff

--- tests/menu_test.py
+++ tests/menu_test.py
@@ -1,20 +1,10 @@
-# bot.py
-import os
-import random
-
-import discord
-from discord.ext import commands
-from dotenv import load_dotenv
 from datetime import date
 
 from bs4 import BeautifulSoup
 import requests
 import re
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
 
 MENSA_HTWG = 'https://seezeit.com/essen/speiseplaene/mensa-htwg/'
 
@@ -23,6 +13,7 @@ def validateIngr(sup):
     return bool(re.match("\((\d\w?,?)+\)", sup))
 
 def attr_lookup(attribute):
+    print(attribute)
     lookup = {
             "Veg": "Vegetarisch",
             "Vegan": "Vegan",
@@ -40,33 +31,7 @@ def attr_lookup(attribute):
                 result.append(lookup[attr])
     return ", ".join(result) if result else ""
 
-# Commands
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-
-@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-        'I\'m the human form of the 💯 emoji.',
-        'Love, it sustains you. It’s like oatmeal.',
-        'You’re under arrest for ruining something perfect!',
-        'Every time you talk, I hear that sound that plays when Pac-Man dies.',
-        'I asked them if they wanted to embarrass you, and they instantly said yes.',
-        'Fine, but in protest, I’m walking over there extremely slowly!',
-        'Why don’t you just do the right thing and jump out of a window?'
-    ]
-
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-
-@bot.command(name='mensa', help='Responds with the current menu')
-async def menu(ctx):
+def menu():
     page_response = requests.get(MENSA_HTWG)
     soup = BeautifulSoup(page_response.content, 'html.parser')
     contents = soup.find('div', class_='tx-speiseplan')
@@ -80,12 +45,9 @@ async def menu(ctx):
         # if "16.05." in current_tab:
             current_tab_class = tab.get('class')[1]
             break
-    
-            
-    response = discord.Embed(
-                title="In der Mensa gibt es:",
-                url=MENSA_HTWG
-            )
+    response={
+        "categories": []
+            }
     if not current_tab_class is None:
 
         day = contents.find('div', {"id":current_tab_class})
@@ -99,12 +61,16 @@ async def menu(ctx):
                 if not validateIngr(sup.text): sup.unwrap()
                 else : sup.decompose()
             attribute=menu.find('div', class_='title_preise_2').find('div', class_=attr_class)['class']
-            response.add_field(name=f"{category.text} : {attr_lookup(attribute)}", value=f"{food.text}", inline=True)
+            response["categories"].append({
+                "title": category.text,
+                "value": food.text,
+                "attribute": attr_lookup(attribute)
+                    })
+
     else:
-        response.add_field(name="Heute wohl nix", value="Zu oder so :(")
+        response["error"] = "Heut wohl nix, zu oder so"
 
-    await ctx.send(embed=response)
+    print(response)
 
 
-bot.run(TOKEN)
-
+if __name__ == "__main__": menu()
